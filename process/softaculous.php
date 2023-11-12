e14f5b149ca08ee7b7411c4d216e6a0feefe1599
mahshidmoulaei <56962481+mahshid1378@users.noreply.github.com>
2023-11-12 03:17:28 -0800

    Add files via upload

---
+++ process/softaculous.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+*
+* Vesta Web Interface
+*
+* Copyright (C) 2020 Carter Roeser <carter@cdgtech.one>
+* https://cdgco.github.io/VestaWebInterface
+*
+* Vesta Web Interface is free software: you can redistribute it and/or modify
+* it under the terms of version 3 of the GNU General Public License as published
+* by the Free Software Foundation.
+*
+* Vesta Web Interface is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Vesta Web Interface.  If not, see
+* <https://github.com/cdgco/VestaWebInterface/blob/master/LICENSE>.
+*
+*/
+
+session_set_cookie_params(['samesite' => 'none']); session_start();
+$configlocation = "../includes/";
+if (file_exists( '../includes/config.php' )) { require( '../includes/includes.php'); }  else { header( 'Location: ../install' ); exit(); };
+
+if(base64_decode($_SESSION['loggedin']) == 'true') {}
+else { header('Location: ../login.php'); exit(); }
+
+if(!isset($softaculousurl) || $softaculousurl == ''){ header("Location: ../error-pages/403.html"); exit(); }
+
+if(!isset($_POST['password']) || $_POST['password'] == '') { header("Location: ".strtok($_SERVER['HTTP_REFERER'],'?')."?sft-pass-missing=true"); }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <link href="../css/style.css" rel="stylesheet">
+    </head>
+    <body class="fix-header">
+        <div class="preloader">
+            <svg class="circular" viewBox="25 25 50 50">
+                <circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" />
+            </svg>
+        </div>
+
+        <form id="form" action="<?php echo $url8083; ?>/login/vwi.php" method="post">
+            <?php echo '<input type="hidden" name="user" value="'.$username.'">'; ?>
+            <?php echo '<input type="hidden" name="password" value="'.$_POST['password'].'">'; ?>
+        </form>
+        <script type="text/javascript">
+            document.getElementById('form').submit();
+        </script>
+    </body>
+    <script src="../plugins/components/jquery/jquery.min.js"></script>
+</html>
